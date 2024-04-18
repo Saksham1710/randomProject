bb288043ae0989497661f606da345276b64ced35
Saksham Vasudev <97773012+Saksham1710@users.noreply.github.com>
2024-04-18 10:43:39 -0700

    Update Db.js

--- Server/Db.js
+++ Server/Db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-  const uri = "mongodb://localhost:27017/BookList";
+  const uri = "mongodb+srv://saksham:Saksham%40123@cluster0.j5qv4li.mongodb.net";
 const connectDb = async() => {
   try {
     const connectionInstance = await mongoose.connect(uri,{ useNewUrlParser: true, useUnifiedTopology: true });
